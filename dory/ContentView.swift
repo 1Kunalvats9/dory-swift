9f835791fe3346196aa3e5b1a6f84f6e1153a482
Kunal Vats <1kunalvats9@gmail.com>
2025-12-25 01:41:51 +0530

    Initial Commit

---
+++ dory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dory
+//
+//  Created by Kunal Vats on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dory/doryApp.swift
@@ -0,0 +1,17 @@
+//
+//  doryApp.swift
+//  dory
+//
+//  Created by Kunal Vats on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct doryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
